97e604a96d8e05237fb324b23615e75aa6c8cfb2
rwy <1021996183@qq.com>
2018-04-20 14:17:37 +0800

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <van-nav-bar
       title="首页"
       left-text="返回"
-      right-text="我的"
+      right-text="我的22"
       left-arrow
       @click-left="onClickLeft"
       @click-right="onClickRight"
