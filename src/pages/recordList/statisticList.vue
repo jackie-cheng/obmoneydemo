84668bc2f28049c6e2cb9f7a9099d886e46cba06
jcakie <771810659@qq.com>
2018-07-18 17:37:33 +0800

    交易记录

--- src/pages/recordList/statisticList.vue
+++ src/pages/recordList/statisticList.vue
@@ -4,25 +4,25 @@
     <van-nav-bar title="统计记录" left-arrow @click-left="onClickLeft()"/>
     <!--时间查询-->
     <div class="search_date" >
-      <yd-datetime   v-model="datetimeStar" type="date" style=" background-color: #efeff4;"></yd-datetime>
+      <yd-datetime   v-model="datetimeStar" :start-date="starSevenTime"   type="date" style=" background-color: #efeff4;"></yd-datetime>
       <span style="margin: 0 0.2rem">至</span>
       <yd-datetime   v-model="datetimeEnd" type="date" :start-date="datetimeStar" style=" background-color: #efeff4;"></yd-datetime>
-      <span class="search_but" @click="searchList">查询</span>
+      <span class="search_but" @click="searchList,page=1">查询</span>
     </div>
 <!--总数-->
-<div class="tab_statis">
+<div class="tab_statis" v-if="statisticData&&statisticData!=null">
   <van-tabbar  class="tab_statistic">
     <van-tabbar-item icon="shop">
-      <span>666</span>
+      <span>{{statisticData.betCount}}</span>
       <template slot="icon" slot-scope="props">
        总单数
       </template>
     </van-tabbar-item>
-    <van-tabbar-item icon="chat"><span>8888.54</span>
+    <van-tabbar-item icon="chat"><span>{{statisticData.totleAmt}}</span>
       <template slot="icon" slot-scope="props">
         总下注
       </template></van-tabbar-item>
-    <van-tabbar-item icon="records"><span>-88962.22</span>
+    <van-tabbar-item icon="records"><span>{{statisticData.bonusAmt -statisticData.totleAmt}}</span>
       <template slot="icon" slot-scope="props">
         总盈亏
       </template></van-tabbar-item>
@@ -30,10 +30,10 @@
 
 </div>
     <van-tabs v-model="activeIndex" swipeable>
-      <van-tab v-for="index in 4" :key="index" :title="'北京' + index">
+      <van-tab v-for="(room,index) in roomList" :key="index" :title="room.roomLotteryTicketTypeContext" v-if="roomList&&roomList.length>0">
 
         <!--交易记录-->
-        <div style="height: 80vh" v-if="activeIndex==0">
+        <div>
 
           <table class="ob_pay_record_table" >
             <tr>
@@ -42,16 +42,18 @@
               <th width="30%">金额</th>
               <th width="20%">中奖</th>
             </tr>
-            <tr v-for="n in 10">
-              <td width="30%">2018-4-40</td>
-              <td width="20%">充值</td>
+            <tr v-for="trade in tradeData" v-if="tradeData&&tradeData.length>0">
+              <td width="30%">{{trade.issueApi}}</td>
+              <td width="20%" v-if="(trade.betStr).indexOf('tema')==-1">{{ballType[trade.betStr]}}</td>
+              <td width="20%" v-if="(trade.betStr).indexOf('tema')!=-1">特码 - {{trade.betStr.substr(4,trade.betStr.length)}}</td>
+              <!--<td width="20%">{{trade.issueApi}}</td>-->
               <!--如果为充值，则颜色添加红色，添加样式pay_money-->
-              <td width="30%" class="pay_money">+1000.00</td>
-              <td width="20%">100.00</td>
+              <td width="30%" class="pay_money">{{trade.totleAmt}}</td>
+              <td width="20%">{{trade.issueApi}}</td>
             </tr>
           </table>
         </div>
-        <div style="height: 80vh" v-else>
+        <div style="height: 80vh">
           <div style="margin:0.2rem 0.3rem;color: #666;">暂无记录</div>
         </div>
       </van-tab>
@@ -71,20 +73,142 @@
     name: 'statisticList',
     data () {
       return {
+        loading: false,
+        finished: false,
+
+        roomList:null,
+        statisticData:null,
+        tradeData:null,
+        userData:null,
+        page: 1,
         activeIndex:0,
+        activeRoomNumber:null,
         datetimeStar: '2018-05-11',
-        datetimeEnd:'2017-05-11',
+        starSevenTime:'2018-05-11',
+        datetimeEnd: '2018-05-11',
+        ballType:{
+          'da':'大',
+          'dadan':'大单',
+          'dan':'单',
+          'xiaodan':'小单',
+          'jida':'极大',
+          'xiao':'小',
+          'shuang':'双',
+          'dashuang':'大双',
+          'xiaoshuang':'小双',
+          'jixiao':'极小',
+          'hongbo':'红波',
+          'lanbo':'蓝波',
+          'lvbo':'绿波',
+          'baozi':'豹子',
+          'shunzi':'顺子',
+          'duizi':'对子',
+        },
       }
+
+    },
+    mounted(){
+      const vm = this
+      vm.$watch('activeIndex',()=>{
+          vm.activeRoomNumber=vm.roomList[vm.activeIndex].roomnumber
+        vm.page=1
+        vm.searchList()
+      }, {deep: true})
+
     },
     methods: {
       searchList(){
         const vm = this
+        vm.tradeData=[]
         const toast1 = vm.$toast.loading({
           mask: true,
-          duration: 3000,       // 持续展示 toast
-          message: '加载中...'
+          duration: 5000,       // 持续展示 toast
+          message: ''
         });
-//          vm.$toast('搜索成功');
+        let params={
+          token: vm.userData.token,
+//          gameId: vm.activeRoomNumber,
+          pageNo:vm.page,
+          pageSize:10,
+          startDate:vm.datetimeStar,
+          endDate:vm.datetimeEnd,
+        }
+        let url = '/user/bet/userBetList'
+        vm.$axios.get(url,{params})
+          .then(response => {
+            toast1.clear();
+            if (response.status == 200&&response.data) {
+              if(response.data.statusCode==-100){
+                vm.$dialog.confirm({
+                  message: response.data.resultInfo
+                }).then(() => {
+                  localStorage.removeItem('userInfo')
+                  vm.$router.push('/login')
+                }).catch(() => {
+                  localStorage.removeItem('userInfo')
+                  vm.$router.push('/')
+                });
+              }else{
+                let newList =response.data.resultInfo
+                vm.tradeData =(vm.tradeData||[]).concat(newList)
+                vm.page++
+                vm.loading = false;
+                if ( newList.length == 0) {
+                  vm.finished = true;
+                }
+              }
+
+
+            } else {
+              vm.$toast('获取信息失败');
+            }
+          }).catch(response => {
+          vm.$toast('获取信息失败');
+        })
+
+      },
+      searchAll(){
+        const vm = this
+        const toast1 = vm.$toast.loading({
+          mask: true,
+          duration: 5000,       // 持续展示 toast
+          message: ''
+        });
+        let params={
+          token: vm.userData.token,
+          startDate:vm.datetimeStar,
+          endDate:vm.datetimeEnd,
+          roomNumber:null,
+        }
+        let url = '/user/bet/userBetSum'
+        vm.$axios.get(url,{params})
+          .then(response => {
+            toast1.clear();
+            if (response.status == 200&&response.data) {
+              if(response.data.statusCode==-100){
+                vm.$dialog.confirm({
+                  message: response.data.resultInfo
+                }).then(() => {
+                  localStorage.removeItem('userInfo')
+                  vm.$router.push('/login')
+                }).catch(() => {
+                  localStorage.removeItem('userInfo')
+                  vm.$router.push('/')
+                });
+              }else{
+
+                vm.statisticData =response.data.resultInfo
+
+              }
+
+
+            } else {
+              vm.$toast('获取汇总信息失败');
+            }
+          }).catch(response => {
+          vm.$toast('获取汇总信息失败');
+        })
+
       },
       onClickLeft(){
         this.$router.go(-1)
@@ -93,6 +217,33 @@
     },
     created () {
       const vm = this
+      var dd = new Date();
+      vm.datetimeEnd=dd.toJSON().slice(0,10)
+      let sevenTime= dd.getTime()-1000*60*60*24*7
+      vm.datetimeStar = new Date(sevenTime).toJSON().slice(0,10)
+      vm.starSevenTime=new Date(sevenTime).toJSON().slice(0,10)
+      if(localStorage.getItem('userInfo')){
+        vm.userData = JSON.parse(localStorage.getItem('userInfo'))
+      }
+      //要先获取房间列表
+      let params = {
+        pageNo: 1,
+        pageSize: 20,
+      }
+      vm.$axios.get(`/api/RoomController/queryRoomList`, {params})
+        .then(response => {
+          if (response.status == 200 && response.data) {
+            vm.roomList = response.data.resultInfo
+            vm.activeRoomNumber=vm.roomList[0].roomnumber
+            vm.searchList()
+            vm.searchAll()
+          } else {
+            vm.$toast('获取房间列表失败');
+          }
+        }).catch(response => {
+        toast1.clear();
+      })
+
     },
     components: {},
   }
--- src/pages/recordList/tradeList.vue
+++ src/pages/recordList/tradeList.vue
@@ -6,16 +6,16 @@
       <van-tab v-for="(menu,index) in menuList" :key="index" :title="menu">
 
         <!--交易记录-->
-        <div style="height: 80vh;position: relative" v-if="activeIndex==0">
+        <div>
           <!--时间查询-->
-          <div class="search_date">
+          <div class="search_date" >
             <span style="position: relative">
-                <yd-datetime   v-model="datetimeStar" type="date"></yd-datetime>
+                <yd-datetime   v-model="datetimeStar" :start-date="starSevenTime"   type="date"></yd-datetime>
             </span>
 
             <span style="margin: 0 0.2rem">至</span>
             <yd-datetime   v-model="datetimeEnd" type="date" :start-date="datetimeStar"></yd-datetime>
-            <span class="search_but" @click="searchList">查询</span>
+            <span class="search_but" @click="searchList,page=1">查询</span>
           </div>
           <table class="ob_pay_record_table" >
             <tr>
@@ -24,17 +24,19 @@
               <th width="30%">金额</th>
               <th width="20%">余额</th>
             </tr>
-            <tr v-for="n in 6">
-              <td width="30%">2018-4-40</td>
-              <td width="20%">充值</td>
+
+            <tr v-for="trade in tradeData" v-if="tradeData&&tradeData.length>0">
+              <td width="30%">{{trade.changeTime.slice(0,10)}}</td>
+              <td width="20%">{{allType[trade.betType]}}</td>
               <!--如果为充值，则颜色添加红色，添加样式pay_money-->
-              <td width="30%" class="pay_money">+1000.00</td>
-              <td width="20%">100.00</td>
+              <td width="30%" :class="{pay_money:trade.change.indexOf('-')!=-1}">{{trade.change}}</td>
+              <td width="20%">{{trade.resultBlnc}}</td>
             </tr>
+
           </table>
         </div>
-        <div style="height: 80vh" v-else>
-          <div style="margin:0.2rem 0.3rem;color: #666;">暂无记录</div>
+        <div  v-if="tradeData&&tradeData.length==0">
+          <div style="position: absolute;top: 5rem">暂无记录</div>
         </div>
       </van-tab>
     </van-tabs>
@@ -53,29 +55,142 @@
     name: 'tradeList',
     data () {
       return {
+        loading: false,
+        finished: false,
+
+        tradeData:null,
+        userData:null,
+        page: 1,
         menuList:['全部','充值','提现','投注'],
         activeIndex:0,
+        activeType:null,
         datetimeStar: '2018-05-11',
-        datetimeEnd:'2017-05-11',
+        starSevenTime:'2018-05-11',
+        datetimeEnd: '2018-05-11',
+        allType:{
+          bet:'下注',
+          cancelBet:'取消下注',
+          bonus:'奖金',
+          recharge:'充值',
+          withdraw:'提现',
+        }
       }
     },
+    computed:{
+
+    },
     methods: {
+
+      onLoad() {
+        setTimeout(() => {
+          for (let i = 0; i < 10; i++) {
+            this.list.push(this.list.length + 1);
+          }
+          this.loading = false;
+
+          if (this.list.length >= 40) {
+            this.finished = true;
+          }
+        }, 500);
+      },
       searchList(){
         const vm = this
         const toast1 = vm.$toast.loading({
           mask: true,
-          duration: 3000,       // 持续展示 toast
-          message: '加载中...'
+          duration: 5000,       // 持续展示 toast
+          message: ''
         });
-//          vm.$toast('搜索成功');
+        let params={
+          token: vm.userData.token,
+          pageNo:vm.page,
+          pageSize:10,
+          startDate:vm.datetimeStar,
+          endDate:vm.datetimeEnd,
+          type:vm.activeType,
+        }
+        let url = '/user/bet/getUserMoneyChangeList'
+        vm.$axios.get(url,{params})
+          .then(response => {
+            toast1.clear();
+            if (response.status == 200&&response.data) {
+              if(response.data.statusCode==-100){
+                vm.$dialog.confirm({
+                  message: response.data.resultInfo
+                }).then(() => {
+                  localStorage.removeItem('userInfo')
+                  vm.$router.push('/login')
+                }).catch(() => {
+                  localStorage.removeItem('userInfo')
+                  vm.$router.push('/')
+                });
+              }else{
+                  let newList =response.data.resultInfo
+                vm.tradeData =(vm.tradeData||[]).concat(newList)
+                vm.page++
+                vm.loading = false;
+                if ( newList.length == 0) {
+                  vm.finished = true;
+                }
+              }
+
+
+            } else {
+              vm.$toast('获取余额信息失败');
+            }
+          }).catch(response => {
+          vm.$toast('获取余额信息失败');
+        })
+
       },
+
       onClickLeft(){
         this.$router.go(-1)
       },
 
     },
+    mounted(){
+      const vm = this
+      vm.$watch('activeIndex',()=>{
+       if(vm.activeIndex==0){
+vm.activeType=null
+         vm.tradeData=[]
+         vm.page=1
+         vm.searchList()
+       }else if(vm.activeIndex==1){
+           //充值
+         vm.activeType='recharge'
+         vm.tradeData=[]
+         vm.page=1
+         vm.searchList()
+       }
+       else if(vm.activeIndex==1){
+           //提现
+         vm.activeType='withdraw'
+         vm.tradeData=[]
+         vm.page=1
+         vm.searchList()
+       }
+       else if(vm.activeIndex==3){
+         //投注
+         vm.activeType='bet'
+         vm.tradeData=[]
+         vm.page=1
+         vm.searchList()
+       }
+      }, {deep: true})
+
+    },
     created () {
       const vm = this
+      var dd = new Date();
+      vm.datetimeEnd=dd.toJSON().slice(0,10)
+      let sevenTime= dd.getTime()-1000*60*60*24*7
+      vm.datetimeStar = new Date(sevenTime).toJSON().slice(0,10)
+      vm.starSevenTime=new Date(sevenTime).toJSON().slice(0,10)
+      if(localStorage.getItem('userInfo')){
+        vm.userData = JSON.parse(localStorage.getItem('userInfo'))
+      }
+      vm.searchList()
     },
     components: {},
   }
